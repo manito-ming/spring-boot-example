6ebd4b87e5656c68d00162fc824bf4142c6adfdc
manito-ming <1315915230@qq.com>
2019-11-19 14:47:50 +0800

    ehcache修改注解

--- spring-cache/src/main/java/com/example/ehcache/EhcacheApplication.java
+++ spring-cache/src/main/java/com/example/ehcache/EhcacheApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @ServletComponentScan
 @MapperScan("com.example.ehcache.dao")
 @SpringBootApplication
+@EnableCaching   //缓存的开关
 public class EhcacheApplication {
 
     public static void main(String[] args) {
